57c38b25b79c3cdd7cdc0d1a1f8363662e181017
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-28 12:15:59 +0600

    Create component MenuItem and displayed in markup right-block App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import './App.css';
+import MenuItem from "./components/MenuItem/MenuItem.tsx";
+import food from './assets/food.png';
+import drink from './assets/drink.png';
 
-const App = () => (
-    <>
 
-    </>
-);
+type MenuItemType = {
+    title: string;
+    price: number;
+    image: string;
+}
+
+const App = () => {
+    const MenuElements: MenuItemType[] = [
+        {title: 'Hamburger', price: 80, image: food},
+        {title: 'Coffee', price: 70, image: drink},
+        {title: 'Cheeseburger', price: 90, image: food},
+        {title: 'Tea', price: 50, image: drink},
+        {title: 'Fries', price: 45, image: food},
+        {title: 'Cola', price: 40, image: drink},
+
+    ];
+    return (
+        <>
+            <div className='right-block'>
+                <h2 className='add-items-title'>Add items</h2>
+                <div className='menu-items'>
+                    {MenuElements.map((element) =>
+                        <MenuItem
+                            key={element.title}
+                            title={element.title}
+                            image={element.image}
+                            price={element.price}/>)}
+                </div>
+            </div>
+        </>
+    );
+}
 
 export default App
---
+++ src/components/MenuItem/MenuItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MenuItem.css';
+
+interface MenuItemProps {
+    image:string;
+    title:string;
+    price:number;
+}
+
+const MenuItem:React.FC<MenuItemProps> = ({image, title, price}) => {
+    return (
+        <button type='button' className='menu-item'>
+            <img className='menu-image' src={image} alt={title}/>
+            <div className='item-text'>
+                <h3 className='menu-title'>{title}</h3>
+                <p className='menu-price'>Price: {price} KGS</p>
+            </div>
+        </button>
+    );
+};
+
+export default MenuItem;
